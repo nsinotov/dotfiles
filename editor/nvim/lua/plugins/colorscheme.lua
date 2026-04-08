1c800a9d3ef60be7d2e9b89c5efb05d394271f52
Nikolay Sinotov <n.sinotov@gmail.com>
2026-04-08 15:25:28 +0300

    Nvim colorscheme improvements

--- editor/nvim/lua/plugins/colorscheme.lua
+++ editor/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,25 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { "italic" },
+        functions = { "italic" },
+        types = { "italic" },
+      },
+      custom_highlights = function(colors)
+        return {
+          ["@tag.tsx"] = { fg = colors.pink, italic = false },
+          ["@tag.javascript"] = { fg = colors.pink, italic = false },
+          ["@tag.attribute"] = { italic = false },
+          ["@variable.parameter"] = { italic = true },
+          ["@lsp.type.parameter"] = { italic = true },
+        }
+      end,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = { colorscheme = "catppuccin-mocha" },
