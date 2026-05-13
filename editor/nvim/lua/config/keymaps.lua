0e21bfb2cd14eaf933e41d2324993e7772bef3eb
Nikolay Sinotov <n.sinotov@gmail.com>
2026-05-13 22:24:53 +0300

    Add <leader>yp/<leader>yP to copy file path to clipboard

--- editor/nvim/lua/config/keymaps.lua
+++ editor/nvim/lua/config/keymaps.lua
@@ -5,3 +5,11 @@
 vim.keymap.set("n", "<leader>sF", function()
   Snacks.picker.grep({ regex = false })
 end, { desc = "Grep (fixed string)" })
+
+vim.keymap.set("n", "<leader>yp", function()
+  vim.fn.setreg("+", vim.fn.expand("%:."))
+end, { desc = "Copy relative file path" })
+
+vim.keymap.set("n", "<leader>yP", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Copy absolute file path" })
